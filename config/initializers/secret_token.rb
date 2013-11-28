6a7581fe3f670cccf8c706cbe5f930e781630099
Celso Dantas <celsodantas@gmail.com>
2013-11-27 20:40:15 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Snappify::Application.config.secret_key_base = 'a1b82c9188dd983ab66b62eba8f7081ef08530c0b1439dc1b8d6c767ba63ba692fc0114c6d30928fb389457b53c2e5099605d64383a44bd87573b9d618252de2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Snappify::Application.config.session_store :cookie_store, key: '_snappify_session'
